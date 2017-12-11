ad5f8f865e4f6f804210fb9081f7677690b07748
thomashorrobin <thomasroberthorrobin@gmail.com>
2017-12-11 20:53:39 +1300

    added user model

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package liquidmarket
 
 import (
-	"fmt"
 	"net/http"
-
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/user"
 )
 
 func init() {
@@ -13,22 +9,12 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	
-	c := appengine.NewContext(r)
-	
-	u := user.Current(c)
-
-	if u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
-		return
+	user := User{ 
+		ID: "111307193244795741281",
+		FirstName: "Thomas",
+		LastName: "Horrobin",
+		Email: "thomasroberthorrobin@gmail.com",
 	}
-	
-	fmt.Fprintf(w, "Hello, %v!", u)
-	
+
+	respondWithJSON(w, http.StatusOK, user)
 }
