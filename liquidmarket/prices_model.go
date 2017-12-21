f47f9da260ace1443064de228b58ea9256f73201
thomashorrobin <thomasroberthorrobin@gmail.com>
2017-12-21 17:07:17 +1300

    updated prices query

--- liquidmarket/prices_model.go
+++ liquidmarket/prices_model.go
@@ -1,32 +1,32 @@
 package liquidmarket
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 )
 
 type Market struct {
-    OrganisationName    string    `json:"organisation_name"`
-    ShortName  string `json:"short_name"`
-    TotalShares  int `json:"total_shares"`
-    BuyPrice   float32    `json:"buy_price"`
-    SellPrice   float32    `json:"sell_price"`
+	OrganisationName string  `json:"organisation_name"`
+	ShortName        string  `json:"short_name"`
+	TotalShares      int     `json:"total_shares"`
+	BuyPrice         float32 `json:"buy_price"`
+	SellPrice        float32 `json:"sell_price"`
 }
 
 func GetPrices(db *sql.DB) ([]Market, error) {
-    statement := fmt.Sprintf("SELECT o.`name`, m.`shortname`, m.`total`, 0 as 'buy', 0 as 'sell' FROM markets AS m JOIN organisations AS o ON m.`organisation_id` = o.id;")
-    rows, err := db.Query(statement)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-    markets := []Market{}
-    for rows.Next() {
-        var m Market
-        if err := rows.Scan(&m.OrganisationName, &m.ShortName, &m.TotalShares, &m.BuyPrice, &m.SellPrice); err != nil {
-            return nil, err
-        }
-        markets = append(markets, m)
-    }
-    return markets, nil
+	statement := fmt.Sprintf("SELECT o.`name`, m.`shortname`, m.`total`, off.`price` + off.spread as 'buy', off.`price` - off.spread as 'sell' FROM markets AS m JOIN organisations AS o ON m.`organisation_id` = o.id JOIN offers AS off ON m.`latest_offer_id` = off.id;")
+	rows, err := db.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	markets := []Market{}
+	for rows.Next() {
+		var m Market
+		if err := rows.Scan(&m.OrganisationName, &m.ShortName, &m.TotalShares, &m.BuyPrice, &m.SellPrice); err != nil {
+			return nil, err
+		}
+		markets = append(markets, m)
+	}
+	return markets, nil
 }
