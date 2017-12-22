0bdd6ecf899858292840ea37a5586127065e3fa7
thomashorrobin <thomasroberthorrobin@gmail.com>
2017-12-22 13:12:43 +1300

    added more allowed origins for testing

--- liquidmarket/cors.go
+++ liquidmarket/cors.go
@@ -2,16 +2,17 @@ package liquidmarket
 
 import (
 	"net/http"
+
 	"github.com/rs/cors"
 )
 
 func corsWrapper(h http.Handler) http.Handler {
 	handler := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:5000", "https://enhanced-emblem-188503.firebaseapp.com", "https://demo.liquidmarket.co.nz"},
 		AllowCredentials: true,
-		AllowedHeaders: []string {"Authorization", "X-Requested-With", "Content-Type"},
-		AllowedMethods: []string {"OPTIONS", "GET", "POST", "DELETE", "PUT"},
+		AllowedHeaders:   []string{"Authorization", "X-Requested-With", "Content-Type"},
+		AllowedMethods:   []string{"OPTIONS", "GET", "POST", "DELETE", "PUT"},
 	})
-	
+
 	return handler.Handler(h)
 }
