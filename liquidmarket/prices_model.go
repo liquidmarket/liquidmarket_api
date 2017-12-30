5cea0ede0c32446756231c166c99eae1fac87f63
thomashorrobin <thomasroberthorrobin@gmail.com>
2017-12-30 13:11:44 +1300

    added offer id to prices model

--- liquidmarket/prices_model.go
+++ liquidmarket/prices_model.go
@@ -10,6 +10,7 @@ import (
 
 type Listing struct {
 	ID               int       `json:"id"`
+	OfferID          uuid.UUID `json:"offer_id"`
 	OrganisationName string    `json:"organisation_name"`
 	ShortName        string    `json:"short_name"`
 	MarketMakerID    uuid.UUID `json:"market_maker_id"`
@@ -24,13 +25,14 @@ type Listing struct {
 
 type Price struct {
 	MarketMakerID uuid.UUID `json:"market_maker_id"`
+	ID            uuid.UUID `json:"id"`
 	Price         float32   `json:"price"`
 	Spread        float32   `json:"spread"`
 	OfferedAt     time.Time `json:"offered_at"`
 }
 
 func GetListingsWithPrices(db *sql.DB) ([]Listing, error) {
-	statement := fmt.Sprintf("SELECT m.id, o.`name`, m.`shortname`, m.market_maker_id, mm.name, m.`total`, off.`price` + off.spread as 'buy', off.`price` - off.spread as 'sell', off.`price`, off.spread  FROM markets AS m JOIN organisations AS o ON m.`organisation_id` = o.id JOIN offers AS off ON m.`latest_offer_id` = off.id JOIN marketmakers AS mm ON mm.id = m.market_maker_id;")
+	statement := fmt.Sprintf("SELECT m.id, off.id, o.`name`, m.`shortname`, m.market_maker_id, mm.name, m.`total`, off.`price` + off.spread as 'buy', off.`price` - off.spread as 'sell', off.`price`, off.spread  FROM markets AS m JOIN organisations AS o ON m.`organisation_id` = o.id JOIN offers AS off ON m.`latest_offer_id` = off.id JOIN marketmakers AS mm ON mm.id = m.market_maker_id;")
 	rows, err := db.Query(statement)
 	if err != nil {
 		return nil, err
@@ -39,10 +41,10 @@ func GetListingsWithPrices(db *sql.DB) ([]Listing, error) {
 	listings := []Listing{}
 	for rows.Next() {
 		var m Listing
-		if err := rows.Scan(&m.ID, &m.OrganisationName, &m.ShortName, &m.MarketMakerID, &m.MarketMakerName, &m.TotalShares, &m.BuyPrice, &m.SellPrice, &m.Price, &m.Spread); err != nil {
+		if err := rows.Scan(&m.ID, &m.OfferID, &m.OrganisationName, &m.ShortName, &m.MarketMakerID, &m.MarketMakerName, &m.TotalShares, &m.BuyPrice, &m.SellPrice, &m.Price, &m.Spread); err != nil {
 			return nil, err
 		}
-		priceQuery := fmt.Sprintf("SELECT marketmaker_id, price, spread, offered_at FROM offers WHERE market_id = %d ORDER BY offered_at DESC LIMIT 30", m.ID)
+		priceQuery := fmt.Sprintf("SELECT marketmaker_id, id, price, spread, offered_at FROM offers WHERE market_id = %d ORDER BY offered_at DESC LIMIT 30", m.ID)
 		////
 		priceQueryRows, err := db.Query(priceQuery)
 		if err != nil {
@@ -53,7 +55,7 @@ func GetListingsWithPrices(db *sql.DB) ([]Listing, error) {
 		for priceQueryRows.Next() {
 			var p Price
 			var timeByteArray string
-			if err := priceQueryRows.Scan(&p.MarketMakerID, &p.Price, &p.Spread, &timeByteArray); err != nil {
+			if err := priceQueryRows.Scan(&p.MarketMakerID, &p.ID, &p.Price, &p.Spread, &timeByteArray); err != nil {
 				return nil, err
 			}
 			time, err := time.Parse("2006-01-02 15:04:05", timeByteArray)
